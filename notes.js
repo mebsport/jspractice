5327655510eff3d6baf86e4670c34f0305877d51
Marcelus <mebsport@gmail.com>
2022-01-27 12:57:56 -0500

    finished project (some parts don't work)

---
+++ notes.js
@@ -0,0 +1,29 @@
+// // Select items from DOM
+// // Single element
+// console.log(document.getElementById('my-form'));
+// console.log(document.querySelector('h1'));
+// // Multiple elemtnt
+// console.log(document.querySelectorAll('.item'));
+// console.log(document.getElementsByClassName('item'));
+// console.log(document.getElementsByTagName('li'));
+
+// Edit  items
+//const ul = document.querySelector('.items');
+// ul.remove();
+//ul.firstElementChild.textContent = 'hello';
+//ul.children[1].innerText = 'brad';
+//ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+//const btn = document.querySelector('.btn');
+//btn.style.backround = 'red';
+
+//on button press/hover
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('mouseout', (e) => {
+//   e.preventDefault();
+//   document.querySelector('#my-form').style.backround = '#ccc';
+//   document.querySelector('body').classList.add('bg-dark');
+//   document.querySelector('.items').lastElementChild.innerHTML =
+//     '<h1>Helllo</h1>';
+// });
