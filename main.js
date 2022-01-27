16153bf88bda52ac1a4540a2b18f2f31f76a690b
Marcelus <mebsport@gmail.com>
2022-01-27 12:58:10 -0500

    style

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  if (nameInput.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please Fill Out All Fields';
+    setTimeout(() => msg.remove(), 4500);
+  } else {
+    const li = document.createElement('li');
+    li.appendChild(
+      document.createTextNode(`${nameInput.value} : ${emailInput.value}`)
+    );
+
+    userList.appendChild(li);
+
+    //clear fields
+    nameInput.vale = '';
+    emailInput.value = '';
+  }
+}
